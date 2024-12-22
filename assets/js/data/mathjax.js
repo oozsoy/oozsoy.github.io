0ada61138186bd44c9a54005bd68c2b03dc84b52
oozsoy <oganozsoy@gmail.com>
2024-12-22 22:15:18 +0100

    fix: mathjax

--- assets/js/data/mathjax.js
+++ assets/js/data/mathjax.js
@@ -16,12 +16,6 @@ MathJax = {
     processEscapes: true,      // use \$ to produce a literal dollar sign
     processEnvironments: true, // process \begin{xxx}...\end{xxx} outside math mode
     processRefs: true, 
-    tags: 'all',
-    baseURL:                   // URL for use with links to tags (when there is a <base> tag in effect)
-        (document.getElementsByTagName('base').length === 0) ?
-          '' : (String(document.location).replace(/#.*$/, ''))
-  },
-  svg: {
-    fontCache: 'global',
-  },
+    tags: 'all'
+  }
 };
